accef2872e2f585957fc1b92f7cb971ce075f6cb
Shruti Verma <shrutive1009@gmail.com>
2024-11-13 11:37:08 +0530

    Added crud repository

---
+++ src/repositories/userRepository.js
@@ -0,0 +1,16 @@
+import User from '../schema/user.js';
+import crudRepository from './crudReository.js';
+
+const userRepository = {
+  ...crudRepository(User),
+  getByEmail: async function ({ email }) {
+    const user = await User.findOne({ email });
+    return user;
+  },
+  getByUserName: async function ({ username }) {
+    const user = await User.findOne({ username });
+    return user;
+  }
+};
+
+export default userRepository;
