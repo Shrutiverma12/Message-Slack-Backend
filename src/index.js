775864e4b2fc28bd0690ae0db2ecf084643d83e4
Shruti Verma <shrutive1009@gmail.com>
2024-12-04 01:04:47 +0530

    Added cors for api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import { createServer } from 'http';
 import { StatusCodes } from 'http-status-codes';
@@ -18,6 +19,7 @@ const io = new Server(server);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors());
 app.use('/ui', bullServerAdapter.getRouter());
 
 app.use('/api', apiRouter);
